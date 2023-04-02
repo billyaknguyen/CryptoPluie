fe5fa605f4dbe01c4423b555ec642c2825190b56
Billy Nguyen <billyaknguyen@gmail.com>
2023-04-01 20:11:26 -0400

    added morgan package

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
+app.use(morgan("tiny"));
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
