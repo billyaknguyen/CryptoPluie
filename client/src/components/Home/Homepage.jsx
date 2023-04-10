8ca00cfb9758366651924a321edc0fd06715d932
Billy Nguyen <billyaknguyen@gmail.com>
2023-04-10 02:34:44 -0400

    put coins data into coins file for cleaner code

--- client/src/components/Home/Homepage.jsx
+++ client/src/components/Home/Homepage.jsx
@@ -1,24 +1,10 @@
 import { useRef, useEffect, useState} from "react";
 import Navbar from "../Navbar/Navbar"
+import Coins from "./assets/coins";
 import { HomeContainer, FirstSection, SecondSection,  AnimatedLetter, TitleContainer, Wave, RainCoatImg, SecondSectionTitle, SecondSectionWrapper, CoinsSlider, CoinImg, ThirdSection } from "./HomepageStyles";
-import BitcoinPic from "./assets/bitcoin.svg";
-import Ethereum from "./assets/ethereum.svg"
-import DogeCoin from "./assets/dogecoin.svg"
-import Xrp from "./assets/x.svg"
-import Polygon from "./assets/polygon.svg"
-import Solana from "./assets/solana.svg"
-
 
 const HomePage = () => {
-  const text = "Track your portfolio with CryptoPluie";
-  const coins = [
-{src: BitcoinPic, alt: "Bitcoin"},
-{src: Ethereum, alt: "Ethereum"},
-{src: DogeCoin, alt: "DogeCoin"},
-{src: Xrp, alt: "Xrp"},
-{src: Polygon, alt: "Polygon"},
-{src: Solana, alt: "Solana"}
-]
+const text = "Everything Crypto with CryptoPluie";
 const myRef = useRef()
 const [playCoinAnimation, setPlayCoinAnimation] = useState(false)
 
@@ -26,9 +12,14 @@ useEffect(() => {
 const observer = new IntersectionObserver((entries) => {
 const entry = entries[0];
 setPlayCoinAnimation(entry.isIntersecting)
-},
-)
+}, {
+  threshold : 1
+})
 observer.observe(myRef.current)
+
+return () => {
+  observer.disconnect();
+}
   }, [])
   
   const animatedTitle = text.split('').map((letter, index) => 
@@ -49,7 +40,7 @@ return (
       <SecondSectionWrapper>
         <SecondSectionTitle >Track Cryptocurrency prices</SecondSectionTitle>
         <CoinsSlider ref={myRef}>
-          {coins.map((coin, index) => {
+          {Coins.map((coin, index) => {
             return playCoinAnimation && (
             <CoinImg key={index} src={coin.src} alt={coin.alt} />
             )
---
+++ client/src/components/Home/assets/Coins.js
@@ -0,0 +1,17 @@
+import Bitcoin from "./bitcoin.svg";
+import Ethereum from "./ethereum.svg";
+import DogeCoin from "./dogecoin.svg";
+import Xrp from "./x.svg";
+import Polygon from "./polygon.svg";
+import Solana from "./solana.svg";
+
+const Coins = [
+  { src: Bitcoin, alt: "Bitcoin" },
+  { src: Ethereum, alt: "Ethereum" },
+  { src: DogeCoin, alt: "DogeCoin" },
+  { src: Xrp, alt: "Xrp" },
+  { src: Polygon, alt: "Polygon" },
+  { src: Solana, alt: "Solana" },
+];
+
+export default Coins;
