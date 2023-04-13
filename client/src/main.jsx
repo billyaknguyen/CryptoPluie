c46a9ffa11f0d95b2703fe1ef20deb4cde4f474b
Billy Nguyen <billyaknguyen@gmail.com>
2023-04-13 04:17:22 -0400

    created Auth0 , login, logout

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {Auth0Provider} from "@auth0/auth0-react"
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
+const domain = import.meta.env.VITE_AUTH0_DOMAIN
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+    <Auth0Provider 
+    domain={domain}
+    clientId={clientId}
+    redirectUri = {window.location.origin}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-  </React.StrictMode>,
+    </Auth0Provider>
 )
