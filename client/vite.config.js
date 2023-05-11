f64a08adc282670f95c96732101000ab400f9b18
Billy Nguyen <billyaknguyen@gmail.com>
2023-05-11 04:26:34 -0400

    testing conditional proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:3000/",
+      "/api": isProduction ? "https://crytopluie-api.onrender.com" :"http://localhost:3000/"
     },
   },
   plugins: [react()],
